1670f75e8278180b017bd9e174d811120ca8f21c
jasonsashley <jasonsashley99@gmail.com>
2024-11-19 18:55:32 -0500

    small tweak

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -3,7 +3,6 @@ package projects.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import projects.exception.DbException;
 
 public class DbConnection {
@@ -12,10 +11,6 @@ public class DbConnection {
 	private static final String PASSWORD = "projects";
 	private static final String HOST = "localhost";
 	private static final int PORT = 3306;
-
-	public DbConnection() {
-
-	}
 
 	public static Connection getConnection() {
 		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER,
